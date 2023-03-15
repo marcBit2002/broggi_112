765e7a3a5cd5b91791755570576915d7f152c89a
Victor Guillén Alcaraz <victorguillofficial@gmail.com>
2023-03-15 13:51:09 +0100

    Navbar admin fix

--- resources/views/layouts/admin_template.blade.php
+++ resources/views/layouts/admin_template.blade.php
@@ -12,36 +12,34 @@
 
 <body>
     @include('layouts.navbar')
-    <div class='row row-cols-2'>
-        <div class="col-2 border-end-4 border-primary me-2">
-            <nav class="nav flex-column ms-3">
-                <div class="text-primary mt-4 ms-2 me-4">
-                    <h4><a id="back_arrow" class="me-2" href='{{ url('/menu') }}'><img class="me-2 mb-1"
-                                src="{{ Vite::asset('resources/icons/back_arrow.svg') }}"></a>ADMINISTRACIÓ
-                    </h4>
-                    <hr class="border border-primary border-1 opacity-100 ms-n5 me-n4">
-                </div>
 
-                <div id="elements_container" class="nav-pills flex-column ">
-                    <a class="nav-link mt-3 mb-3" href="#">
-                        <i class="bi bi-bar-chart-fill me-2"></i>Estadístiques</a>
-                    <a class="nav-link mt-3 mb-3" href="#" selected>
-                        <i class="bi bi-people-fill me-2"></i>Usuaris</a>
-                    <a class="nav-link mt-3 mb-3" href="#">
-                        <i class="bi bi-file-earmark-text-fill me-2"></i>Gestió d'expedients</a>
-                    <a class="nav-link mt-3 mb-3" href="#">
-                        <i class="bi bi-fire me-2"></i>Incidents</a>
-                    <a class="nav-link mt-3 mb-3" href="#">
-                        <i class="bi bi-grid-fill me-2"></i>Tipus d'incidents</a>
-                    <a class="nav-link mt-3 mb-3" href="#">
-                        <i class="bi bi-briefcase-fill me-2"></i>Agències</a>
-                </div>
-            </nav>
+    <div id="container">
+        <div id="nav_container">
+            <div class="title_container">
+                <a href='{{ url('/menu') }}' id="back_arrow"><img
+                        src="{{ Vite::asset('resources/icons/back_arrow.svg') }}"></a>
+                <p>ADMINISTRACIÓ</p>
+            </div>
+            <div id="items_contaienr">
+                <a class="nav-link my-3" href="#">
+                    <i class="bi bi-bar-chart-fill me-2"></i>Estadístiques</a>
+                <a class="nav-link my-3" href="#">
+                    <i class="bi bi-people-fill me-2"></i>Usuaris</a>
+                <a class="nav-link my-3" href="#" selected>
+                    <i class="bi bi-file-earmark-text-fill me-2"></i>Gestió d'expedients</a>
+                <a class="nav-link my-3" href="#">
+                    <i class="bi bi-fire me-2"></i>Incidents</a>
+                <a class="nav-link my-3" href="#">
+                    <i class="bi bi-grid-fill me-2"></i>Tipus d'incidents</a>
+                <a class="nav-link my-3" href="#">
+                    <i class="bi bi-briefcase-fill me-2"></i>Agències</a>
+            </div>
         </div>
-        <div class='col-9 mt-5 ms-5'>
+        <div id="content">
             @yield('contenido')
         </div>
     </div>
+
 </body>
 
 </html>
