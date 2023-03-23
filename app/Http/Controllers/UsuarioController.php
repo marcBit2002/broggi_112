d13ac3fb69d2195b5d4357114d735fa45cd25050
marc-examen <marclazaromarti@gmail.com>
2023-03-23 13:41:20 +0100

    Test

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -18,19 +18,10 @@ class UsuarioController extends Controller
 
         $usuaris = Usuario::orderBy('nom', 'ASC')->paginate(6);
         $roles = TiposUsuario::orderBy('id', 'ASC')->get();
-        // $navLateral = 'usuaris';
-
-        // $view1 = view('layouts/admin_template', compact('navLateral'));
 
         $activeNav = "usuaris";
 
         return view('usuaris', compact('usuaris', 'roles', 'activeNav'));
-    }
-
-    public function miMetodo()
-    {
-        $seleccionActual = 'usuaris';
-        return view('layouts/admin_template', compact('seleccionActual'));
     }
 
     public function create()
