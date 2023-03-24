b1b86338d47caf158a6d8de6178dd2eba8df4757
marc-examen <marclazaromarti@gmail.com>
2023-03-24 11:39:12 +0100

    Created Expedient API

---
+++ app/Http/Controllers/Api/ExpedientController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Expedient;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Resources\ExpedientResource;
+
+class ExpedientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $expedients = Expedient::all();
+        return ExpedientResource::collection($expedients);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Expedient  $expedient
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Expedient $expedient)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Expedient  $expedient
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Expedient $expedient)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Expedient  $expedient
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Expedient $expedient)
+    {
+        //
+    }
+}
