0b825d2bf4587184845f05cf7b7a6bfa3a4b318e
Victor Guillén Alcaraz <victorguillofficial@gmail.com>
2023-03-29 12:21:29 +0200

    Revert "Merge branch 'main' of https://github.com/marcBit2002/broggi_112"
    This reverts commit f4265de649f3fe80b7519db419baae8cc0835ee0, reversing
    changes made to a1308753a7b0b95e27645368e19f039b29611033.

--- resources/js/components/pagina.vue
+++ resources/js/components/pagina.vue
@@ -9,7 +9,6 @@
                     src="/broggi_112/public/assets/icons/arki.svg"
                     class="arki"
                     data-bs-toggle="popover"
-                    data-bs-trigger="focus"
                     data-bs-title="Help Box"
                     data-bs-content="And here's some amazing content. It's very engaging. Right?"
                 />
@@ -59,40 +58,6 @@
         </div>
         <div class="content" v-if="this.tab == 2">
             <h1 id="title">Localització</h1>
-            <div class="input_group">
-                <label for="estaCatalnya">Està a Catalunya?</label>
-                <input type="checkbox" id="estaCatalnya" value="" />
-                <input type="checkbox" id="noEstaCatalnya" value="" />
-                <img
-                    src="/broggi_112/public/assets/icons/arki.svg"
-                    class="arki"
-                    data-bs-toggle="popover"
-                    data-bs-trigger="focus"
-                    data-bs-title="Help Box"
-                    data-bs-content="And here's some amazing content. It's very engaging. Right?"
-                />
-            </div>
-            <div class="input_group">
-                <label>Tipus localització:</label>
-                <input type="radio" id="age1" name="age" value="30" />
-                <label for="age1">Carrer</label>
-                <input type="radio" id="age2" name="age" value="60" />
-                <label for="age2">Punt Singular</label>
-                <input type="radio" id="age3" name="age" value="100" />
-                <label for="age3">Carretera</label>
-                <input type="radio" id="age3" name="age" value="100" />
-                <label for="age3">ENTITAT/Població</label>
-                <input type="radio" id="age3" name="age" value="100" />
-                <label for="age3">Comarca</label>
-                <img
-                    src="/broggi_112/public/assets/icons/arki.svg"
-                    class="arki"
-                    data-bs-toggle="popover"
-                    data-bs-trigger="focus"
-                    data-bs-title="Help Box"
-                    data-bs-content="And here's some amazing content. It's very engaging. Right?"
-                />
-            </div>
         </div>
         <div class="content" v-if="this.tab == 3">
             <h1 id="title">Tipificació</h1>
@@ -147,20 +112,12 @@ export default {
         insertCarta() {
             alert("CARTA DONE!");
         },
-        removePopovers() {
-            const popovers = document.querySelectorAll(".popover");
-            popovers.forEach((popover) => {
-                popover.remove();
-            });
-        },
     },
     updated() {
-        this.removePopovers();
-
+        console.log("cambio");
         const popoverTriggerList = document.querySelectorAll(
             '[data-bs-toggle="popover"]'
         );
-
         const popoverList = [...popoverTriggerList].map(
             (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
         );
@@ -223,11 +180,9 @@ export default {
         margin-bottom: 2rem;
 
         label {
-            &:first-child {
-                margin-right: 2rem;
-                width: 130px;
-                text-align: right;
-            }
+            margin-right: 2rem;
+            width: 130px;
+            text-align: right;
         }
 
         input {
