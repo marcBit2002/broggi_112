d6925154058762fd704608a5be2e53496e6e7ab9
marc-examen <marclazaromarti@gmail.com>
2023-04-18 21:59:41 +0200

    Mapa agencies almost done

--- resources/js/components/mapa.vue
+++ resources/js/components/mapa.vue
@@ -3,36 +3,56 @@
         <input
             type="text"
             class="buscador form-control"
-            placeholder="Introdueix la direcció"
+            placeholder="Introduce la dirección"
             v-model="direccion"
         />
         <div id="map"></div>
-        <div class="agencies"></div>
+        <div class="agencies">
+            <h2>Agencias seleccionadas:</h2>
+            <div class="agencies-list">
+                <div
+                    v-for="agencia in agenciasSeleccionadas"
+                    :key="agencia.id"
+                    class="agency-pill"
+                >
+                    <span>{{ agencia.nom }} - {{ agencia.carrer }}</span>
+                    <button @click="deseleccionarAgencia(agencia)">×</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+import axios from "axios";
+
 export default {
     data() {
         return {
-            direccion: "",
+            direccion: "Plaça Urquinaona",
+            agencias: [],
+            mapboxClient: null,
+            agenciasSeleccionadas: [],
         };
+    },
+
+    created() {
+        mapboxgl.accessToken =
+            "pk.eyJ1IjoibWFyY2JpdDIwMDIiLCJhIjoiY2xnZzcyZjk5MDh2OTNxcnJoOGtjYXJkNCJ9.di7gnS5p1QxJO0VS1yuwZg";
+        this.mapboxClient = mapboxSdk({
+            accessToken: mapboxgl.accessToken,
+        });
+    },
+    mounted() {
+        this.cargarAgencias();
     },
     watch: {
         direccion() {
-            this.cargarMapa();
+            this.cargarAgencias();
         },
-    },
-    mounted() {
-        this.cargarMapa();
     },
     methods: {
         cargarMapa() {
-            mapboxgl.accessToken =
-                "pk.eyJ1IjoibWFyY2JpdDIwMDIiLCJhIjoiY2xnZzcyZjk5MDh2OTNxcnJoOGtjYXJkNCJ9.di7gnS5p1QxJO0VS1yuwZg";
-            const mapboxClient = mapboxSdk({
-                accessToken: mapboxgl.accessToken,
-            });
-            mapboxClient.geocoding
+            this.mapboxClient.geocoding
                 .forwardGeocode({
                     query: this.direccion,
                     autocomplete: false,
@@ -54,17 +74,112 @@ export default {
 
                     const map = new mapboxgl.Map({
                         container: "map",
-                        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-                        style: "mapbox://styles/mapbox/streets-v9",
-                        // style: "mapbox://styles/mapbox/navigation-day-v1",
+                        style: "mapbox://styles/mapbox/light-v9",
+                        // style: "mapbox://styles/mapbox/streets-v9",
                         center: feature.center,
                         zoom: 15,
                         interactive: true,
                     });
 
-                    // Create a marker and add it to the map.
                     new mapboxgl.Marker().setLngLat(feature.center).addTo(map);
+
+                    this.agregarMarcadoresAgencias(map);
                 });
+        },
+        cargarAgencias() {
+            axios.get("agencia").then((response) => {
+                this.agencias = response.data;
+                this.cargarMapa();
+            });
+        },
+        agregarMarcadoresAgencias(map) {
+            this.agencias.forEach((agencia) => {
+                const direccion = agencia.carrer;
+                this.mapboxClient.geocoding
+                    .forwardGeocode({
+                        query: direccion,
+                        autocomplete: false,
+                        limit: 1,
+                    })
+                    .send()
+                    .then((response) => {
+                        if (
+                            !response ||
+                            !response.body ||
+                            !response.body.features ||
+                            !response.body.features.length
+                        ) {
+                            console.error("Invalid response:");
+                            console.error(response);
+                            return;
+                        }
+                        const feature = response.body.features[0];
+
+                        let iconClass;
+                        if (agencia.nom.toLowerCase().includes("bombers")) {
+                            iconClass = "bi bi-fire";
+                        } else if (
+                            agencia.nom.toLowerCase().includes("policia")
+                        ) {
+                            iconClass = "bi bi-shield-fill";
+                        } else if (
+                            agencia.nom.toLowerCase().includes("hospital")
+                        ) {
+                            iconClass = "bi bi-heart-fill";
+                        }
+
+                        const markerElement = document.createElement("div");
+                        markerElement.className = "marker";
+                        markerElement.innerHTML = `<i class="${iconClass}"></i>`;
+
+                        const marker = new mapboxgl.Marker(markerElement)
+                            .setLngLat(feature.center)
+                            .addTo(map);
+
+                        marker.getElement().addEventListener("click", () => {
+                            const estaSeleccionada =
+                                this.agenciasSeleccionadas.includes(agencia);
+                            const buttonText = estaSeleccionada
+                                ? "Deseleccionar"
+                                : "Seleccionar";
+                            const buttonColor = estaSeleccionada
+                                ? "red"
+                                : "green";
+                            const popup = new mapboxgl.Popup({
+                                offset: 25,
+                            }).setHTML(
+                                `<h3>${agencia.nom}</h3><p>${agencia.carrer}</p><button style="background-color: ${buttonColor}">${buttonText}</button>`
+                            );
+                            popup.addTo(map);
+                            marker.setPopup(popup);
+
+                            popup.on("open", () => {
+                                popup
+                                    .getElement()
+                                    .querySelector("button").onclick = () => {
+                                    if (estaSeleccionada) {
+                                        this.deseleccionarAgencia(agencia);
+                                    } else {
+                                        this.seleccionarAgencia(agencia);
+                                    }
+                                    popup.remove();
+                                };
+                            });
+                        });
+                    });
+            });
+        },
+
+        seleccionarAgencia(agencia) {
+            if (!this.agenciasSeleccionadas.includes(agencia)) {
+                this.agenciasSeleccionadas.push(agencia);
+            }
+        },
+        deseleccionarAgencia(agencia) {
+            const index = this.agenciasSeleccionadas.indexOf(agencia);
+            if (index !== -1) {
+                this.agenciasSeleccionadas.splice(index, 1);
+            }
         },
     },
 };
@@ -80,6 +195,7 @@ export default {
     overflow: hidden;
     padding: 0;
 }
+
 .buscador {
     margin-bottom: 10px;
 }
@@ -100,6 +216,39 @@ export default {
     box-sizing: border-box;
     padding: 10px;
     overflow: auto;
+}
+
+.agencies h2 {
+    margin-bottom: 10px;
+}
+
+.agencies-list {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.agency-pill {
+    background-color: #eee;
+    padding: 10px;
+    border-radius: 20px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+    display: flex;
+    align-items: center;
+}
+
+.agency-pill span {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
+.agency-pill button {
+    background-color: transparent;
+    border: none;
+    font-size: 20px;
+    line-height: 1;
+    margin-left: 10px;
 }
 
 @media only screen and (max-width: 980px) {
